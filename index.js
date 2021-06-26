9c08364276fc8b35c43640d7ea33e6e66d8abd20
Edgar Alandete <edgar.sanchez.alandete@gmail.com>
2021-06-26 09:25:38 +0200

    Create login endpoint create controller auth routes create auth controller implement test add axios and debug dependency

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 
 const morgan = require('morgan');
-const debug = require('debug');
-// const cors = require('cors');
+const debug = require('debug')('app');
 require('dotenv').config();
-// const authRoutes = require('./routes/auth.routes');
-// const dieticiansRouter = require('./routes/dieticians.routes');
+const authRoutes = require('./src/routes/auth.routes');
 
 const port = process.env.PORT || 1905;
 
@@ -13,6 +11,6 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
-// app.use('/', authRoutes);
+app.use('/api', authRoutes);
 
 app.listen(port, debug(`server is running on port ${port}`));
---
+++ src/controllers/authController.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+function authController() {
+  async function login(req, res) {
+    const { client_id, client_secret } = req.body;
+    try {
+      const { data } = await axios.post(process.env.LOGIN_API, { client_id, client_secret });
+      res.json(data);
+    } catch (error) {
+      res.status(401);
+      res.send('Invalid secret or client id');
+    }
+  }
+
+  return {
+    login,
+  };
+}
+
+module.exports = authController;
