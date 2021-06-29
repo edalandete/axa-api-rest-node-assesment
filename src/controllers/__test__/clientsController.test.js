0b79f189ce4799f5f52e51d94a80ea9fceabc9ab
Edgar Alandete <edgar.sanchez.alandete@gmail.com>
2021-06-29 17:08:33 +0200

    Update clients tests

--- src/controllers/__test__/clientsController.test.js
+++ src/controllers/__test__/clientsController.test.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const {
-  getAll, getById
+  getAll, getById, getPoliciesFromClientId
 } = require('../clientsController')();
 
 jest.mock('axios');
@@ -66,7 +66,7 @@ describe('Given clientsController', () => {
       status: jest.fn()
     };
 
-    describe('And the promise is resolved', () => {
+    describe('And the promise is resolved with a client matching the id', () => {
       test('Then the client with the id 1 should be sent as response', async () => {
         const clients = {
           data:
@@ -89,10 +89,78 @@ describe('Given clientsController', () => {
       });
     });
 
+    describe('And the promise is resolved without a client matching the id', () => {
+      test('Then the client with the id 1 should be sent as response', async () => {
+        const clients = {
+          data:
+            []
+        };
+        axios.get.mockResolvedValueOnce(clients);
+        await getById(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(404);
+      });
+    });
+
     describe('And the promise is rejected', () => {
       test('Then the status code 401 should be sent', async () => {
         axios.mockRejectedValueOnce();
         await getById(req, res);
+        expect(res.status).toHaveBeenCalledWith(401);
+      });
+    });
+  });
+  describe('When it is called with a getPoliciesFromClientId function', () => {
+    const req = {
+      params: {
+        id: '1'
+      },
+      json: jest.fn(),
+      send: jest.fn()
+    };
+
+    const res = {
+      json: jest.fn(),
+      send: jest.fn(),
+      status: jest.fn()
+    };
+
+    describe('And the promise is resolved with client having policies', () => {
+      test('Then the client with the id 1 should be sent as response', async () => {
+        const clients = {
+          data:
+            [
+              {
+                id: '12',
+                name: 'Edgar',
+                clientId: '1'
+              }
+            ]
+        };
+        axios.get.mockResolvedValueOnce(clients);
+        await getPoliciesFromClientId(req, res);
+
+        expect(res.json).toHaveBeenCalledWith([{ clientId: '1', id: '12', name: 'Edgar' }]);
+      });
+    });
+
+    describe('And the promise is resolved with client that has no policies', () => {
+      test('Then the client with the id 1 should be sent as response', async () => {
+        const clients = {
+          data:
+            []
+        };
+        axios.get.mockResolvedValueOnce(clients);
+        await getPoliciesFromClientId(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(404);
+      });
+    });
+
+    describe('And the promise is rejected', () => {
+      test('Then the status code 401 should be sent', async () => {
+        axios.mockRejectedValueOnce();
+        await getPoliciesFromClientId(req, res);
         expect(res.status).toHaveBeenCalledWith(401);
       });
     });
