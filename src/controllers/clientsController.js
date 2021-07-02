76317ceb923ce4eeeee5d0ca86134f0b0b01adae
Edgar Alandete <edgar.sanchez.alandete@gmail.com>
2021-07-02 12:46:12 +0200

    Add documentation for clientsController and commonHelpers

--- src/controllers/clientsController.js
+++ src/controllers/clientsController.js
@@ -4,11 +4,33 @@ const {
 } = require('../helpers/commonHelpers');
 const { localStorage } = require('../providers/cache-provider');
 
+/**
+ *
+ * @returns All the functions used in the controller
+ */
+
 function clientsController() {
   const token = localStorage.getItem('token');
   const type = localStorage.getItem('type');
   const role = localStorage.getItem('role');
   const clientId = localStorage.getItem('clientId');
+
+  /**
+   * This function gets all clients if the role is admin
+   * This function gets one client if the role is user and the user id is the
+   * same as the logged user
+   *
+   * @param {object} req Request API Call includes:
+   *  - query {object}
+   *    - page:   {number} page to show
+   *    - limit:  {number} registers per page to show
+   *    - name:   {string} client name
+   *
+   * @param {object} res Response API Call
+   *  - Possible status:
+   *   - 200: OK
+   *   - 401: Unauthorized
+   */
 
   async function getAll(req, res) {
     const { page, limit, name } = req.query;
@@ -29,6 +51,21 @@ function clientsController() {
       res.send('Unauthorized');
     }
   }
+
+  /**
+   *
+   * Function that returns the client data matching the id from params
+   *
+   * @param {*} req Request API Call includes:
+   *  - params: {object}
+   *    - id: {number} client id
+   * @param {*} res Response API Call
+   *   - Possible status:
+   *    - 200: OK
+   *    - 401: Unauthorized
+   *    - 403: Forbidden
+   *    - 404: Not found
+   */
   async function getById(req, res) {
     const { id } = req.params;
 
@@ -52,6 +89,21 @@ function clientsController() {
       res.send('Unauthorized');
     }
   }
+
+  /**
+   *
+   * Function that returns all the policies matching one clientId
+   *
+   * @param {object} req Request API call includes
+   *  - params: {object}
+   *    - id: {number} client id
+   * @param {object} res Response API call
+   *   - Possible status:
+   *    - 200: OK
+   *    - 401: Unauthorized
+   *    - 403: Forbidden
+   *    - 404: Not found
+   */
   async function getPoliciesFromClientId(req, res) {
     const { id } = req.params;
     const requestHeaders = { headers: { Authorization: `${type} ${token}` } };
